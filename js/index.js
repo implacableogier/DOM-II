0e2d68d24dc44a505d5df3f862fc62ee07b5553d
Jason <jason-cummings@lambdastudents.com>
2020-11-01 19:07:08 -0800

    add 2 event listeners (mouseenter/leave) to the bus img

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+
+//selector for the bus div
+const busImg = document.querySelector('.container .intro img')
+//bus grows larger when mouse enters frame
+busImg.addEventListener('mouseenter', function() {
+    busImg.style.transform = "scale(1.1)";
+} );
+//bus returns to original size when leaving frame
+busImg.addEventListener('mouseleave', function() {
+    busImg.style.transform = "scale(1.0)";
+} );
+
+
